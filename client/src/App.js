9b3b9acf85e4d9eb975242aca8155de72f62a62a
Arpit2607 <manasvitech01@gmail.com>
2024-05-15 18:53:36 +0530

    login

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import CompanyRegistration from './components/auth/CompanyRegistration';
+import Login from './components/auth/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<CompanyRegistration/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>
-      Login
+    <div className="bg-gradient-to-br from-blue-500 to-yellow-500 h-screen flex justify-center items-center text-white font-montserrat">
+      <div className="login-card p-8 w-full max-w-md flex flex-col">
+        <div className="header mb-12">
+          <div className="logo rounded-full w-32 h-32 flex justify-center items-center mx-auto mb-4 bg-white bg-opacity-10">
+            <div className="text-white text-6xl">
+              <FaUserAlt />
+            </div>
+          </div>
+        </div>
+        <div className="form">
+          <div className="form-field username relative mb-6">
+            <div className="icon absolute bg-white text-black left-0 top-0  flex justify-center items-center w-10 h-10 rounded-full">
+              <FaUserAlt />
+            </div>
+            <input
+              type="text"
+              placeholder="Username"
+              className="pl-12 pr-4 py-2 w-full bg-opacity-10 border border-white rounded-lg focus:bg-white focus:text-red-600 focus:outline-none transition duration-300"
+            />
+          </div>
+          <div className="form-field password relative mb-6">
+            <div className="icon absolute bg-white text-black left-0 top-0 flex justify-center items-center w-10 h-10 rounded-full">
+              <FaLock />
+            </div>
+            <input
+              type="password"
+              placeholder="Password"
+              className="pl-12 pr-4 py-2 w-full bg-opacity-10 border border-white rounded-lg focus:bg-white focus:text-red-600 focus:outline-none transition duration-300"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-white text-red-600 py-3 px-4 w-full rounded-full uppercase font-bold mb-8 focus:outline-none transition duration-300 hover:bg-red-600 hover:text-white"
+          >
+            Login
+          </button>
+          <div className="text-white text-center">
+            Don't have an account?{" "}
+            <Link href="#" className="font-bold hover:text-yellow-400">
+              Sign Up Now
+            </Link>
+          </div>
+          <div className="text-white text-center">
+            <Link href="#" className="text-white font-bold hover:text-yellow-400">
+              Forgot password?
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
