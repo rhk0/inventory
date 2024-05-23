30d68044a0dbe1e7e6f829d6464137a5fdd9955f
Arpit2607 <manasvitech01@gmail.com>
2024-05-23 17:29:41 +0530

    COMPANY photo frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CompanyRegistration from "./components/auth/CompanyRegistration";
 import Login from "./components/auth/Login";
-
+import Registraion from "./components/auth/Registraion";
+import ForgotPassword from "./components/auth/ForgotPassword";
+import Otpverification from "./components/auth/Otpverification";
+import ResetPassword from "./components/auth/ResetPassword.js"
 import { AdminProtectedRoute } from "./components/Routes/AdminProtectedRoute.js";
 import AdminRoutes from "./components/Routes/AdminRoute.js";
 
@@ -10,12 +14,19 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>} />
+          <Route path="/" element={<Login />} />
+          <Route path="/registration" element={<Registraion />} />
+          <Route path="/forgetpassword" element={<ForgotPassword />} />
+          <Route path="/otpverification" element={<Otpverification />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
+          <Route
+            path="/CompanyRegistration"
+            element={<CompanyRegistration />}
+          />
 
-
-          
-          <Route path="/admin" element={<AdminProtectedRoute/>}>
-            <Route path="/admin/dashboard/*" element={<AdminRoutes/>} />
+          {/* admin protected routes */}
+          <Route path="/admin" element={<AdminProtectedRoute />}>
+            <Route path="/admin/*" element={<AdminRoutes />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -1,15 +1,9 @@
 import React from "react";
 import { useState } from "react";
-import {  Routes, Route } from "react-router-dom";
-// import AdminDashboard from "../pages/admin/AdminDashboard";
-import Registration from "../auth/Registraion.js";
-import ForgotPassword from "../auth/ForgotPassword.js";
-import OtpVerification from "../auth/Otpverification.js";
-import ResetPassword from "../auth/ResetPassword.js";
-import CompanyRegistration from "../auth/CompanyRegistration.js";
-import Header from "../pages/admin/Header.js";
-import Home from "../pages/admin/Home.js";
-import Sidebar from "../pages/admin/Sidebar.js";
+import { Routes, Route } from "react-router-dom";
+import Header from "../pages/admin/Header";
+import Home from "../pages/admin/Home";
+import Sidebar from "../pages/admin/Sidebar";
 const AdminRoute = () => {
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
@@ -19,20 +13,15 @@ const AdminRoute = () => {
 
   return (
     <div className="grid-container">
-    <Header OpenSidebar={OpenSidebar} />
-    <Sidebar
-      openSidebarToggle={openSidebarToggle}
-      OpenSidebar={OpenSidebar}
-    />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/registration" element={<Registration />} />
-      <Route path="/forgetpassword" element={<ForgotPassword />} />
-      <Route path="/otpverification" element={<OtpVerification />} />
-      <Route path="/resetPassword" element={<ResetPassword />} />
-      <Route path="/CompanyRegistration" element={<CompanyRegistration />} />
-    </Routes>
-  </div>
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
