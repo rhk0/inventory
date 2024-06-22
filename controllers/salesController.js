b7ea52f4bda751c740a02ddc1f154ef04ef2b7e9
Arpit2627 <manasvitech01@gmail.com>
2024-06-22 12:46:25 +0530

    all Quotation  sales controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -12,30 +12,32 @@ export const createSalesController = async (req, res) => {
       taxType,
       billingAddress,
       shippingAddress,
-      Items
-    } = req.body;  
-   
- 
-   const requiredFields = [
-      "date",
-      "quotationNo",
-      "selectCustomer",
-      "reverseCharge",
-      "placeOfSupply",
-      "paymentsTerms",
-      "dueDtae",
-      "taxType",
-      "billingAddress",
-      "shippingAddress",
-      "Items"
-    ];
-    const missingFields = requiredFields.filter((field) => !req.body[field]);
-    if (missingFields.length > 0) {
-      return res.status(400).send({
-        message: "Required fields are missing",
-        missingFields: missingFields,
-      });
-    }
+      Items,
+      taxAmount,
+      totalAmount
+    } = req.body;
+
+    // const requiredFields = [
+    //   "date",
+    //   "quotationNo",
+    //   "selectCustomer",
+    //   "reverseCharge",
+    //   "placeOfSupply",
+    //   "paymentsTerms",
+    //   "dueDtae",
+    //   "taxType",
+    //   "billingAddress",
+    //   "shippingAddress",
+    //   "Items",
+    // ];
+    // const missingFields = requiredFields.filter((field) => !req.body[field]);
+    // if (missingFields.length > 0) {
+    //   return res.status(400).send({
+    //     message: "Required fields are missing",
+    //     missingFields: missingFields,
+    //   });
+    // }
+
     const rows = Items.map((rowData) => ({
       itemName: rowData.itemName,
       hsnCode: rowData.hsnCode,
@@ -45,9 +47,7 @@ export const createSalesController = async (req, res) => {
       sgst: rowData.sgst,
       igst: rowData.igst,
       total: rowData.total,
-     }));
-
-  
+    }));
 
     try {
       const newProduct = new salesQuotationModel({
@@ -62,6 +62,8 @@ export const createSalesController = async (req, res) => {
         billingAddress,
         shippingAddress,
         rows,
+        taxAmount,
+        totalAmount
       });
       const savedProduct = await newProduct.save();
       res.status(201).send({
@@ -73,5 +75,80 @@ export const createSalesController = async (req, res) => {
     }
   } catch (error) {
     res.status(500).send({ error: "Server error", message: error.message });
+  }
+};
+
+export const getAllSalesQuotationCOntroller = async (req, res) => {
+  try {
+    const response = await salesQuotationModel.find();
+
+    if (!response) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Data not found" });
+    }
+
+    return res
+      .status(200)
+      .send({ success: true, message: "Data  found", response });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal Server Issue" });
+  }
+};
+
+export const deletSalesQuotationByIDController = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const deleteData = await salesQuotationModel.findByIdAndDelete({ _id });
+
+    if (!deleteData) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Data not found" });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Quotaion Deleted SUccessfully",
+      deleteData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal Server Issue" });
+  }
+};
+
+export const updateQuotationByIDController = async (req, res) => {
+  try {
+    const { _id } = req.params;
+    const updateData = req.body;
+
+    const updatedQuotation = await salesQuotationModel.findByIdAndUpdate(
+      _id,
+      updateData,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedQuotation) {
+      return res
+        .status(404)
+        .send({ success: false, message: "Data not found" });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Quotation updated successfully",
+      updatedQuotation,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ success: false, message: "Internal Server Issue" });
   }
 };
--- routes/salesQuationRoute.js
+++ routes/salesQuationRoute.js
@@ -1,11 +1,16 @@
-import express from "express"
-import { createSalesController } from "../controllers/salesController.js";
+import express from "express";
+import {
+  createSalesController,
+  deletSalesQuotationByIDController,
+  getAllSalesQuotationCOntroller,
+  updateQuotationByIDController,
+} from "../controllers/salesController.js";
 
 const router = express.Router();
 
-router.post("/createSalesQuotaition",createSalesController);
-// router.get("/manageStaff",manageStaffController)
-// router.get("/manageSingleStaff/:_id",manageSingleStaffController)
-// router.delete("/deletestaff/:_id", deleteStaffController);
+router.post("/createSalesQuotaition", createSalesController);
+router.get("/getAllSalesQuotation", getAllSalesQuotationCOntroller);
+router.put("/updateSalesQuotation/:_id", updateQuotationByIDController);
+router.delete("/deleteSalesQuotation/:_id", deletSalesQuotationByIDController);
 
 export default router;
