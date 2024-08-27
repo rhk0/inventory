94471f5098d7a6877c7e985424730653a58601d4
dheerendra <prajapatidheerendra45@gmail.com>
2024-08-27 11:58:30 +0530

    Resolved merge conflicts

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@
 // EMAIL_USER = manasvistaff.dharma@gmail.com
 // EMAIL_PASS = pbqu tgfp ojyu uzcv
 import React from 'react';
-
+     
 const AddSupplierForm = () => {
   return (
     <div className="max-w-4xl mx-auto p-4 bg-gray-100 rounded-lg">
@@ -13,7 +13,8 @@ const AddSupplierForm = () => {
       <form className="space-y-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           <div>
-            <label className="block text-gray-700">Name</label>
+
+            <label className="block text-gray-700">Name</label>yghhgt
             <input type="text" className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300" />
           </div>
           <div>
