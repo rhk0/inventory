c65da443e6be71b482bc24de4437ca84920db54c
dheerendra <prajapatidheerendra45@gmail.com>
2024-09-02 17:03:17 +0530

    sales estimate  crud

--- client/src/components/pages/admin/sales/CreateSalesEstimate.js
+++ client/src/components/pages/admin/sales/CreateSalesEstimate.js
@@ -10,6 +10,7 @@ const CreateSalesEstimate = () => {
   const [placeOfSupply, setPlaceOfSupply] = useState("");
   const [paymentTerms, setPaymentTerms] = useState("");
   const [dueDate, setDueDate] = useState("");
+  const [otherChargesDescriptions,setotherChargesDescriptions]=useState("");
   const [transportDetails, setTransportDetails] = useState({
     receiptDocNo: "",
     dispatchedThrough: "",
