0c1ea3e692b9d0106b734f8a7e62fce5961a9699
dheerendra <prajapatidheerendra45@gmail.com>
2024-09-11 18:40:48 +0530

    SLAES ESTIMATE backend

--- routes/salesEstimateRoute.js
+++ routes/salesEstimateRoute.js
@@ -2,7 +2,6 @@ import express from "express";
 import formidable from "express-formidable";
 
 import { createSalesEstimateController, deleteSalesEstimateController, getSalesEstimateByIdController, getSalesEstimatesController, updateSalesEstimateController } from "../controllers/SalesEstimateController.js";
-import { deletSalesInvoiceByIDController, getAllSalesInvoiceCOntroller } from "../controllers/salesInvoiceController.js";
 
 const router = express.Router();
 
@@ -11,9 +10,5 @@ router.get("/getAllSalesEstimatet",  getSalesEstimatesController);
 router.get("/getAllSalesEstimatetByID/:_id",  getSalesEstimateByIdController);
 router.delete("/deleteSalesEstimatetByID/:_id",  deleteSalesEstimateController);
 router.put("/updateSalesEstimatetByID/:_id",  updateSalesEstimateController);
-
-
-
-
 
 export default router;
--- routes/salesQuationRoute.js
+++ routes/salesQuationRoute.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
-  createSalesController,
+  // createSalesController,
   deletSalesQuotationByIDController,
   getAllSalesQuotationCOntroller,
   updateQuotationByIDController,
 } from "../controllers/salesController.js";
 
+
 const router = express.Router();
 
-router.post("/createSalesQuotaition", createSalesController);
+// router.post("/createSalesQuotaition", createSalesInvoiceController);
 router.get("/getAllSalesQuotation", getAllSalesQuotationCOntroller);
 router.put("/updateSalesQuotation/:_id", updateQuotationByIDController);
 router.delete("/deleteSalesQuotation/:_id", deletSalesQuotationByIDController);
